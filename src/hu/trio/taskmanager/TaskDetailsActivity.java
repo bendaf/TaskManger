49894cf96017d66cfa8df4d69bd5731d9f2d60ca
bendaf <bendaf92@gmail.com>
2015-01-15 20:33:34 +0100

    Add taskdetailsActivity and path for CategoryEdit

---
+++ src/hu/trio/taskmanager/TaskDetailsActivity.java
@@ -0,0 +1,13 @@
+package hu.trio.taskmanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TaskDetailsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.act_task_details);
+	}
+}
