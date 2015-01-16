1c82a5e978104f5dff0951822c3eaa4304c45a74
bendaf <bendaf92@gmail.com>
2015-01-16 01:00:09 +0100

    Remove view recycleing because the colors wasn't changeing

--- src/hu/trio/taskmanager/CategoryArrayAdapter.java
+++ src/hu/trio/taskmanager/CategoryArrayAdapter.java
@@ -25,13 +25,13 @@ public class CategoryArrayAdapter extends BaseAdapter {
 	LayoutInflater inflater;
 	ArrayList<Category> categories = new ArrayList<>(); 
 	Category all;
-	
+
 	public CategoryArrayAdapter(Context context, ArrayList<Category> categories) {
 		inflater = LayoutInflater.from(context);
 		this.categories = categories;
 		all = new Category(context.getString(R.string.all));
 	}
-
+	
 	@Override
 	public int getCount() {
 		return categories.size() + 1;
@@ -53,17 +53,17 @@ public class CategoryArrayAdapter extends BaseAdapter {
 	public View getView(int position, View convertView, ViewGroup parent) {
 		position--; //because the 0th is our own category
 		VH categoryView;
-		if (convertView == null) {
+//		if (convertView == null) {
             convertView = inflater.inflate(R.layout.category_item, parent, false);
             categoryView = new VH();
             categoryView.tvTitle = (TextView) convertView.findViewById(R.id.tv_categoryTitle);
             categoryView.vInnerCircle = (View) convertView.findViewById(R.id.v_categoryInnerCircle);
             categoryView.vOuterCircle = (View) convertView.findViewById(R.id.v_categoryOuterCircle);
-            convertView.setTag(categoryView);
+//            convertView.setTag(categoryView);
 
-        } else {
-        	categoryView = (VH) convertView.getTag();
-        }
+//        } else {
+//        	categoryView = (VH) convertView.getTag();
+//        }
 		try{
 //			Log.d("erdekel", categories.get(position).getTitle().toString());
 			categoryView.tvTitle.setText(categories.get(position).getTitle().toString());
